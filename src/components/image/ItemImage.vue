3cecf75d5eb9d4574de7a3a845a80d6731e9700f
Andrey <andrey@aaee.by>
2024-11-03 14:25:52 +0300

    Add title to images

--- src/components/image/ItemImage.vue
+++ src/components/image/ItemImage.vue
@@ -1,5 +1,5 @@
 <template>
-    <img class="item-mage" :src="itemImage.src" width="32" height="32" :alt="itemImage.alt">
+    <img class="item-mage" :src="itemImage.src" width="32" height="32" :alt="itemImage.alt" :title="itemImage.alt">
 </template>
 
 <script setup>
